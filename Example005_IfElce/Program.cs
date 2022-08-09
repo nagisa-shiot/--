0ba500c3b458dd804cca3242e8526e543c304fdf
Shchiptsov Danil <bondar-seryy@mail.ru>
2022-08-09 20:18:04 +0800

    Условие

---
+++ Example005_IfElce/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "ксюша")
+{
+    Console.WriteLine("Ура,это же Ксюша!!!!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
